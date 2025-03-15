6baf1e18af8977a15990700dfbfba198d2aace5d
34francois <morland.fran@gmail.com>
2025-03-15 08:24:06 +0100

    Update Apollon_revision.py

--- Apollon_revision.py
+++ Apollon_revision.py
@@ -64,7 +64,7 @@ st.markdown(
         border: 1px solid #ccc;
         padding: 20px;
         border-radius: 10px;
-        background-color: #dec237;
+        background-color: #f7f7f7;
         box-shadow: 5px 5px 10px #888888;
         display: flex;
         align-items: center;
