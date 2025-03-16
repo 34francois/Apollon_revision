7892616db845ff8b2c73270285929ae7cec430c1
34francois <morland.fran@gmail.com>
2025-03-16 11:22:15 +0100

    Update Apollon_revision.py

--- Apollon_revision.py
+++ Apollon_revision.py
@@ -61,6 +61,6 @@ with st.sidebar:
     
             # Append the new row to the DataFrame
             st.session_state.df = pd.concat([st.session_state.df, pd.DataFrame([new_row])], ignore_index=True)
-    st.dataframe(st.session_state.df)
+            st.dataframe(st.session_state.df)
 
 
